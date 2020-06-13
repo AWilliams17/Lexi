24faf76a780abaa1eb123477b9a9aacbd79b50df
Austin Williams <awilliams17412@gmail.com>
2020-06-12 20:31:07 -0500

    -renamed package: lexi -> words words_api: -removed all method stubs -added get_word method stub created word module: -added Word class stub

--- lexi.py
+++ lexi.py
@@ -11,31 +11,31 @@ def about():
     click.echo(F"About:")
 
 
-@cli.command(help="Usage: 'lexi define {word}' ~ Looks up the definition for the passed word.")
+@cli.command(help="Usage: 'words define {word}' ~ Looks up the definition for the passed word.")
 @click.argument(F"word")
 def define(word):
     click.echo(F"definition of {word}: ")
 
 
-@cli.command(help="Usage: 'lexi anto {word}' ~ List antonyms for the passed word.")
+@cli.command(help="Usage: 'words anto {word}' ~ List antonyms for the passed word.")
 @click.argument(F"word")
 def anto(word):
     click.echo("antonym of {word}: ")
 
 
-@cli.command(help="Usage: 'lexi syn {word}' ~ List synonyms for the passed word.")
+@cli.command(help="Usage: 'words syn {word}' ~ List synonyms for the passed word.")
 @click.argument(F"word")
 def syn(word):
     click.echo(F"synonym of {word}: ")
 
 
-@cli.command(help="Usage: 'lexi tense {word}' ~ List present, future, and past tenses for the passed word.")
+@cli.command(help="Usage: 'words tense {word}' ~ List present, future, and past tenses for the passed word.")
 @click.argument("word")
 def tense(word):
     click.echo(F"tense of {word}: ")
 
 
-@cli.command(help="Usage: 'lexi lexicon {word}' ~ Outputs all information on the word.")
+@cli.command(help="Usage: 'words lexicon {word}' ~ Outputs all information on the word.")
 @click.argument("word")
 def lex(word):
     click.echo(F"lexicon of {word}: ")
